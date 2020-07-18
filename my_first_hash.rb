96339a58661491d797324befc5ad81a77c535d1a
john beltrez <beltrezjohn@gmail.com>
2020-07-18 12:27:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,10 +1,8 @@
-'require'
+'require pry'
 
 def my_hash
   my_hash = { "kid" => "jordan", "wife" => "maryann"}
-  binding.pry# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  binding.pry
 end
 
 
